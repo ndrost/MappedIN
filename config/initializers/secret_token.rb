8ec03a2821b5e7804f9cb36b4422fdcb43b25ac7
Nick Drost <nicholas.drost@gmail.com>
2012-08-28 22:56:17 -0500

    Initial Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mappedin2::Application.config.secret_token = 'ca884ddd6ab3ecb975b7ac38f4d325a12e4f8a360348e4600083944ceb5c29cfe1f2832c40fdad6f62e63c1edb080233c475697d2cda89a6bfa7cdc97fbe6c76'
