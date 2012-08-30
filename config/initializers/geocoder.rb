ee9d7a5c9b0e61373e3871417faea8f5a2aaa42d
Nick Drost <ndrost@salesforce.com>
2012-08-30 14:07:58 -0500

    Redis Config

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,5 @@
 # caching (see below for details)
-   Geocoder::Configuration.cache = Redis.new(:host => 'cod.redistogo.com', :port => '9255', :password => 'c3917eda45953051e439db4e2441e81f', :username => 'redistogo')
+   Geocoder::Configuration.cache = Redis.new(:host => 'clingfish.redistogo.com', :port => '9255', :password => 'c3917eda45953051e439db4e2441e81f', :username => 'redistogo')
 #   Geocoder::Configuration.cache = Redis.new
 #   Geocoder::Configuration.cache_prefix = "..."
 #   Geocoder::Configuration.lookup = :yahoo
