9751622c0517b03a9856a0eddfa88a46c3fdf5b0
Nick Drost <ndrost@salesforce.com>
2012-08-31 19:54:38 -0500

    UI changes, added logout, performance tweak

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,4 @@
 # caching (see below for details)
-   Geocoder::Configuration.cache = Redis.new(:host => 'cod.redistogo.com', :port => '9469', :password => 'f7f468e09bce8d5137d1d4922b4a6f4a', :username => 'redistogo')
-#   Geocoder::Configuration.cache = Redis.new
+   Geocoder::Configuration.cache = Redis.new
    Geocoder::Configuration.cache_prefix = "..."
    Geocoder::Configuration.lookup = :yahoo
